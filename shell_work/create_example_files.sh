cb8bfbad83f5f27ef81129fffefe5471ce0f1b41
Chris <christoph.hoeppke@gmail.com>
2018-06-25 14:27:09 +0100

    Updates

--- shell_work/create_example_files.sh
+++ shell_work/create_example_files.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+for x in 0 1 2 3 4 5 6 7 8 9
+do
+    touch examplefile$x
+done
 
-echo "Test in a file" > examplefile.txt
+for x in 0 1 2 3
+do
+    mkdir exampledir$x
+done
+touch exampledir1/examplefileA
+touch .hiddenexamplefile
+echo "Test in a file" > exampledir1/examplefile.txt
